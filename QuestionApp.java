935759d2e983e5355a0a58f2912e985cbaee8d48
Oscar McGovern O'Byrne <36077674+x173636253@users.noreply.github.com>
2019-09-24 12:26:01 +0100

    Add files via upload
    Finished version of a three team Java project from December 2018

---
+++ QuestionApp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package questionapp;
+
+/**
+ *
+ * @author x17363253
+ */
+public class QuestionApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        QuestionaireGUI myGUI=new QuestionaireGUI();
+        myGUI.setVisible(true);
+    }
+    
+}
